e6a43006c557b4344870bf6fa13a8410292f7314
luger-mano <lucas.germanysantos@gmail.com>
2025-12-23 00:03:57 -0300

    Atributos adicionados para determinar diferentes filas, estoque e preço.

---
+++ depthstudy/src/main/java/com/in/depthstudy/constants/RabbitMQConstants.java
@@ -0,0 +1,6 @@
+package com.in.depthstudy.constants;
+
+public class RabbitMQConstants {
+    public static final String STOCK_QUEUE = "STOCK";
+    public static final String PRICE_QUEUE = "PRICE";
+}
