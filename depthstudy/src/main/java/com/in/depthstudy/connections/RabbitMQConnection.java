504f6682566528c4ddc9b98cc01ce104489d9d01
luger-mano <lucas.germanysantos@gmail.com>
2025-12-23 00:02:41 -0300

    Metódos criados para adicionar relacionamento entre filas e exchange, criar filas e direciona-las para direct exchange

---
+++ depthstudy/src/main/java/com/in/depthstudy/connections/RabbitMQConnection.java
@@ -0,0 +1,59 @@
+package com.in.depthstudy.connections;
+
+import com.in.depthstudy.constants.RabbitMQConstants;
+import jakarta.annotation.PostConstruct;
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class RabbitMQConnection {
+
+    private static final String EXCHANGE_NAME = "amq.direct";
+    private AmqpAdmin amqpAdmin;
+
+    public RabbitMQConnection(AmqpAdmin amqpAdmin) {
+        this.amqpAdmin = amqpAdmin;
+    }
+
+
+    private Queue queue(String queueName) {
+        return new Queue(queueName, true, false, false);
+    }
+
+    private DirectExchange directExchange() {
+        return new DirectExchange(EXCHANGE_NAME, true, false);
+    }
+
+    private Binding binding(Queue queue, DirectExchange exchange) {
+        return new Binding(queue.getName(), Binding.DestinationType.QUEUE, exchange.getName(), queue.getName(), null);
+    }
+
+    @PostConstruct
+    private void add() throws IOException {
+        Queue stockQueue = this.queue(RabbitMQConstants.STOCK_QUEUE);
+        Queue priceQueue = this.queue(RabbitMQConstants.PRICE_QUEUE);
+
+        DirectExchange exchange = this.directExchange();
+
+        Binding stockBind = this.binding(stockQueue, exchange);
+        Binding priceBind = this.binding(priceQueue, exchange);
+
+        // Create Queues
+        this.amqpAdmin.declareQueue(stockQueue);
+        this.amqpAdmin.declareQueue(priceQueue);
+
+        // Create Exchange
+        this.amqpAdmin.declareExchange(exchange);
+
+        // Create Binds
+        this.amqpAdmin.declareBinding(stockBind);
+        this.amqpAdmin.declareBinding(priceBind);
+
+    }
+}
