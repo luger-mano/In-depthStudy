9d42294847688edbb17571aef276d369d033ce6e
luger-mano <lucas.germanysantos@gmail.com>
2025-12-29 20:32:08 -0300

    Adicionei uma trativa para desserializar e serializar uma mensagem utilizando ObjectMapper

--- stockconsumer/src/main/java/com/microservice/stockconsumer/consumer/StockConsumer.java
+++ stockconsumer/src/main/java/com/microservice/stockconsumer/consumer/StockConsumer.java
@@ -4,14 +4,19 @@ import dto.StockDTO;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import constants.RabbitMQConstants;
 import org.springframework.stereotype.Component;
+import tools.jackson.databind.ObjectMapper;
 
 @Component
 public class StockConsumer {
     @RabbitListener(queues = RabbitMQConstants.STOCK_QUEUE,
             containerFactory = "rabbitListenerContainerFactory")
-    private void consumer(StockDTO dto){
-        System.out.println(dto.getProductCode());
-        System.out.println(dto.getAmount());
+    private void consumer(String message) throws InterruptedException {
+        StockDTO stockDTO = new ObjectMapper().readValue(message, StockDTO.class);
+
+        System.out.println(stockDTO.getProductCode());
+        System.out.println(stockDTO.getAmount());
         System.out.println("----------------");
+
+        Thread.sleep(12000);
     }
 }
