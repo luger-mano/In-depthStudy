dd86ea81481d0a907e828e455bf772f8af14d563
luger-mano <lucas.germanysantos@gmail.com>
2025-12-29 20:31:38 -0300

    Adicionei uma trativa para desserializar e serializar uma mensagem utilizando ObjectMapper

--- depthstudy/src/main/java/com/in/depthstudy/service/RabbitMQService.java
+++ depthstudy/src/main/java/com/in/depthstudy/service/RabbitMQService.java
@@ -1,8 +1,10 @@
 package com.in.depthstudy.service;
 
+import org.springframework.amqp.AmqpException;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import tools.jackson.databind.ObjectMapper;
 
 @Service
 public class RabbitMQService {
@@ -10,7 +12,16 @@ public class RabbitMQService {
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
+    @Autowired
+    private ObjectMapper mapper;
+
     public void sendMessage(String queueName, Object message){
-        this.rabbitTemplate.convertAndSend(queueName, message);
+        try{
+            String jsonMessage = mapper.writeValueAsString(message);
+            this.rabbitTemplate.convertAndSend(queueName, jsonMessage);
+
+        } catch (AmqpException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
