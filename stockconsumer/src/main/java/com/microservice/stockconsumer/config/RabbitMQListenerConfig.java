72074f40c9b072302bdd96610e4300a19659e15c
luger-mano <lucas.germanysantos@gmail.com>
2026-01-04 23:50:20 -0300

    Tratamento de erros fatais e error handler para gerar log

--- stockconsumer/src/main/java/com/microservice/stockconsumer/config/RabbitMQListenerConfig.java
+++ stockconsumer/src/main/java/com/microservice/stockconsumer/config/RabbitMQListenerConfig.java
@@ -1,11 +1,16 @@
 package com.microservice.stockconsumer.config;
 
+import com.microservice.stockconsumer.exceptions.CustomErrorStrategy;
+import com.microservice.stockconsumer.exceptions.TreatmentErrorHandler;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.ConditionalRejectingErrorHandler;
+import org.springframework.amqp.rabbit.listener.FatalExceptionStrategy;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.util.ErrorHandler;
 
 @Configuration
 public class RabbitMQListenerConfig {
@@ -16,17 +21,29 @@ public class RabbitMQListenerConfig {
     }
 
     @Bean
-    public SimpleRabbitListenerContainerFactory  rabbitListenerContainerFactory(
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(
             ConnectionFactory connectionFactory,
             MessageConverter jacksonMessageConverter
-    ){
+    ) {
         SimpleRabbitListenerContainerFactory factory =
                 new SimpleRabbitListenerContainerFactory();
 
         factory.setConnectionFactory(connectionFactory);
         factory.setPrefetchCount(4);
+//        factory.setErrorHandler(new TreatmentErrorHandler());
+        factory.setErrorHandler(errorHandler());
         factory.setMessageConverter(jacksonMessageConverter);
 
         return factory;
+    }
+
+    @Bean
+    public FatalExceptionStrategy customErrorStrategy(){
+        return new CustomErrorStrategy();
+    }
+
+    @Bean
+    public ErrorHandler errorHandler(){
+        return new ConditionalRejectingErrorHandler(customErrorStrategy());
     }
 }
