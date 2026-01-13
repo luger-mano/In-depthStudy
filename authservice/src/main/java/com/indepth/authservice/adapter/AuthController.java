4e8523f4807e28e0c0182180fe18519de6e1b27f
luger-mano <lucas.germanysantos@gmail.com>
2026-01-12 21:54:50 -0300

    feat: controller para criação e login de usuário

---
+++ authservice/src/main/java/com/indepth/authservice/adapter/AuthController.java
@@ -0,0 +1,27 @@
+package com.indepth.authservice.adapter;
+
+
+import com.indepth.authservice.adapter.in.request.UserCredentialRequestDTO;
+import com.indepth.authservice.adapter.in.response.UserCredentialResponseDTO;
+import com.indepth.authservice.domain.ports.in.SaveUserUseCasePort;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth/signup")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final SaveUserUseCasePort saveUserUseCasePort;
+
+    @PostMapping
+    public ResponseEntity<UserCredentialResponseDTO> registerUser(@RequestBody UserCredentialRequestDTO userRequestDTO) {
+        UserCredentialResponseDTO user = saveUserUseCasePort.execute(userRequestDTO);
+        return ResponseEntity.ok(user);
+    }
+
+}
---
+++ authservice/src/main/java/com/indepth/authservice/adapter/TokenController.java
@@ -0,0 +1,59 @@
+package com.indepth.authservice.adapter;
+
+import com.indepth.authservice.adapter.in.request.LoginRequestDTO;
+import com.indepth.authservice.adapter.in.response.LoginResponseDTO;
+import com.indepth.authservice.adapter.out.entities.Role;
+import com.indepth.authservice.domain.ports.in.FindUserByEmailUseCasePort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Instant;
+import java.util.stream.Collectors;
+
+@RestController
+public class TokenController {
+
+    private final JwtEncoder jwtEncoder;
+    private final FindUserByEmailUseCasePort findUserByEmailUseCasePort;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    public TokenController(JwtEncoder jwtEncoder, FindUserByEmailUseCasePort findUserByEmailUseCasePort, BCryptPasswordEncoder passwordEncoder) {
+        this.jwtEncoder = jwtEncoder;
+        this.findUserByEmailUseCasePort = findUserByEmailUseCasePort;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDTO> login(@RequestBody LoginRequestDTO requestDTO) {
+        var user = findUserByEmailUseCasePort.execute(requestDTO.getEmail());
+        if (user == null || !user.isLoginCorrect(requestDTO, passwordEncoder)) {
+            throw new BadCredentialsException("Usuário ou Senha inválido.");
+        }
+        Instant now = Instant.now();
+        long expiresIn = 300L;
+
+        String scopes = user.getRoles()
+                .stream()
+                .map(Role::getName)
+                .collect(Collectors.joining(" "));
+
+        JwtClaimsSet claims = JwtClaimsSet.builder()
+                .issuer("auth-service")
+                .subject(user.getUserId().toString())
+                .issuedAt(now)
+                .expiresAt(now.plusSeconds(expiresIn))
+                .claim("scope", scopes)
+                .build();
+
+        String jwtValue = jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+
+        return ResponseEntity.ok(new LoginResponseDTO(jwtValue, expiresIn));
+    }
+}
