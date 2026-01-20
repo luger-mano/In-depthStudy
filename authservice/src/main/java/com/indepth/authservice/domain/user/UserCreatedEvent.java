3653eb0584ecf6023d7eb9736931f73b1ad1411a
luger-mano <lucas.germanysantos@gmail.com>
2026-01-20 00:25:49 -0300

    feat: Criei DTO para enviar mensagem para criação de usuário com dados do publisher auth-service para o listener user-service

--- authservice/src/main/java/com/indepth/authservice/domain/user/UserCreatedEvent.java
+++ authservice/src/main/java/com/indepth/authservice/domain/user/UserCreatedEvent.java
@@ -1,4 +1,43 @@
 package com.indepth.authservice.domain.user;
 
+import java.util.UUID;
+
 public class UserCreatedEvent {
+
+    private UUID authId;
+    private String email;
+    private String name;
+
+    public UserCreatedEvent(UUID authId, String email, String name) {
+        this.authId = authId;
+        this.email = email;
+        this.name = name;
+    }
+
+    public UserCreatedEvent() {
+    }
+
+    public UUID getAuthId() {
+        return authId;
+    }
+
+    public void setAuthId(UUID authId) {
+        this.authId = authId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
